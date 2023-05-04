8d520023727a3e79314d54d99b145e7398455503
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-04 10:45:19 +0530

    learn-terraform

---
+++ server.tf
@@ -0,0 +1,86 @@
+data "aws_ami" "centos" {
+  owners      = [973714476881]
+  most_recent = true
+  name_regex  = "centos-8-Devops-Practice"
+}
+
+data "aws_security_group" "allow_all" {
+  name = "allow_all"
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+
+    mongodb = {
+    name = "mongodb"
+    instance_type = "t3.small"
+    }
+
+
+    Catalogue = {
+    name = "catalogue"
+    instance_type = "t3.small"
+    }
+
+
+    redis = {
+    name = "redis"
+    instance_type = "t3.small"
+    }
+
+
+    user = {
+    name = "user"
+    instance_type = "t3.small"
+    }
+
+
+    cart = {
+    name = "cart"
+    instance_type = "t3.small"
+    }
+
+
+    mysql = {
+    name = "mysql"
+    instance_type = "t3.small"
+    }
+
+
+
+    shipping = {
+    name = "shipping"
+    instance_type = "t3.small"
+    }
+
+
+    rabbitmq = {
+    name = "rabbitmq"
+    instance_type = "t3.small"
+    }
+
+
+    payment = {
+    name = "payment"
+    instance_type = "t3.small"
+    }
+
+  }
+}
+
+
+    resource "aws_route_53_" "frontend" {
+      for_each = var.components
+    ami      = data.aws_ami.centos.image_id
+      instance_type = each.value["instance_type"]
+      vpc_security_group_ids = [data.aws_security_group.allow_all.id]
+
+
+      tags = {
+        Name = each.value["name"]
+      }
+      }
