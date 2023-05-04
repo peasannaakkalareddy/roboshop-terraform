7aac9d917d8c7e60699dd05e5d7f8c6f48e91a0f
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-04 11:13:17 +0530

    learn-terraform

--- server.tf
+++ server.tf
@@ -74,7 +74,7 @@ variable "components" {
 }
 #1
 
-    resource "aws_route_53_" "frontend" {
+    resource "aws_instance" "instance" {
       for_each = var.components
     ami      = data.aws_ami.centos.image_id
       instance_type = each.value["instance_type"]
