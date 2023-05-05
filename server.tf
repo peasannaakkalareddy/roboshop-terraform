b15772f3b5f739fd4dd18d5ef52e9f736a70141a
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-05 18:37:10 +0530

    learn-terraform

--- server.tf
+++ server.tf
@@ -5,7 +5,7 @@ data "aws_ami" "centos" {
 }
 
 data "aws_security_group" "allow-all" {
-  name = "allow-all"
+  name = "allow-all1"
 }
 
 variable "instance_type" {
@@ -14,7 +14,7 @@ variable "instance_type" {
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "frontend"
@@ -32,7 +32,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "mongodb"
@@ -50,7 +50,7 @@ resource "aws_route53_record" "mongodb" {
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "catalogue"
@@ -68,7 +68,7 @@ resource "aws_route53_record" "catalogue" {
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "user"
@@ -87,7 +87,7 @@ resource "aws_route53_record" "user" {
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "redis"
@@ -104,7 +104,7 @@ resource "aws_route53_record" "redis" {
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "cart"
@@ -121,7 +121,7 @@ resource "aws_route53_record" "cart" {
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "mysql"
@@ -139,7 +139,7 @@ resource "aws_route53_record" "mysql" {
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "shipping"
@@ -156,7 +156,7 @@ resource "aws_route53_record" "shipping" {
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "rabbitmq"
@@ -174,7 +174,7 @@ resource "aws_route53_record" "rabbitmq" {
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group_allow-all.id ]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "payment"
