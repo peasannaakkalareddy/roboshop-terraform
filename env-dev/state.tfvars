340f71f7f8562812af2fe30edf5e51fb3ffcb1b1
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-08 15:31:11 +0530

    learn-terraform

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "akp999"
+key    = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,7 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = "akp999"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
+
   }
 }
