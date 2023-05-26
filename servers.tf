4bdaf200d9107041f8d0d7a7b6e3b1197c9a476c
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-26 22:27:29 +0530

    roboshop - ansible

--- servers.tf
+++ servers.tf
@@ -10,8 +10,9 @@ module "database-servers" {
 }
 
 module "app-servers" {
-  for_each = var.app_servers
+
   depends_on = [module.database-servers]
+  for_each = var.app_servers
   source = "./module"
   component_name = each.value["name"]
   env = var.env
