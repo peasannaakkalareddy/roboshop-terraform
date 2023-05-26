994e27cf225bc99ad8816d45c37f3a830e70f1f1
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-05-26 18:52:56 +0530

    roboshop - ansible

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
+  vpc_security_group_ids = [ data.aws_security_group.allow_all.id ]
 
   tags = {
     Name = local.name
