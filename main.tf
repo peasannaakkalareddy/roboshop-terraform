6141a3146fd419a7a8bf897785425517b12e6a21
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-10 20:52:02 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -8,10 +8,32 @@ module "vpc" {
   env           = var.env
 }
 #
-module "web" {
+module "app" {
   source = "git::https://github.com/peasannaakkalareddy/tf-module-app.git"
 
-  for_each      = var.app
-  instance_type = each.value["instance_type"]
-  subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  for_each         = var.app
+  instance_type    = each.value["instance_type"]
+  name             = each.value["name"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
+
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 }
+
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_app_cidr" {}
+variable "bastion_cidr" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "tags" {}
