dc784c2a21aa0f04a8dc4735cb2da0cc9d18ff9f
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-10 18:12:38 +0530

    roboshop - ansible

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
 output "vpc" {
-  value = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  value = lookup(lookup(module.vpc, "main", null), "subnets", null)
 }
-
