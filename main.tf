54fb23d19003423664db53c08a74f9b86ac90d91
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-11 16:08:40 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -44,3 +44,20 @@ module "docdb" {
   vpc_id  = local.vpc_id
   kms_arn = var.kms_arn
 }
+
+module "rds" {
+  source = "git::https://github.com/peasannaakkalareddy/tf-module-rds.git"
+
+  for_each       = var.rds
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+  engine_version = each.value["engine_version"]
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+
+
+  tags    = local.tags
+  env     = var.env
+  vpc_id  = local.vpc_id
+  kms_arn = var.kms_arn
+}
