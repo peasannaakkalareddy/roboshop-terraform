c419e51ad18b7913cb32a61b51e6ec2a0553c743
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-14 16:32:25 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -24,13 +24,14 @@ module "app" {
   app_port          = each.value["app_port"]
   listener_priority = each.value["listener_priority"]
   dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
   parameters        = each.value["parameters"]
 
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
   listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
-  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
+
 
   env          = var.env
   bastion_cidr = var.bastion_cidr
