67ab81361ebb4de40eeea769e80eabce841cf13b
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-15 21:57:14 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "app" {
 
   env          = var.env
   bastion_cidr = var.bastion_cidr
-  tags         = merge(local.tags, { Monitor = "true" })
+  #tags         = merge(local.tags, { Monitor = "true" })
   tags         = local.tags
   domain_name  = var.domain_name
   domain_id    = var.domain_id
