4fbaa8ab9ae6b483947d23c26bcdcdb86b97b5f6
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-16 11:13:48 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -11,38 +11,38 @@ module "vpc" {
   default_vpc_rtid = var.default_vpc_rtid
 }
 #
-#module "app" {
-#  depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitmq, module.alb]
-#  source = "git::https://github.com/peasannaakkalareddy/tf-module-app.git"
-#
-#  for_each          = var.app
-#  instance_type     = each.value["instance_type"]
-#  name              = each.value["name"]
-#  desired_capacity  = each.value["desired_capacity"]
-#  max_size          = each.value["max_size"]
-#  min_size          = each.value["min_size"]
-#  app_port          = each.value["app_port"]
-#  listener_priority = each.value["listener_priority"]
-#  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
-#  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
-#  parameters        = each.value["parameters"]
-#
-#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
-#  listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
-#
-#
-#  env          = var.env
-#  bastion_cidr = var.bastion_cidr
-#  #tags         = merge(local.tags, { Monitor = "true" })
-#  tags         = local.tags
-#  domain_name  = var.domain_name
-#  domain_id    = var.domain_id
-#  kms_arn      = var.kms_arn
-#  monitor_cidr = var.monitor_cidr
-#
-#}
+module "app" {
+  depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitmq, module.alb]
+  source = "git::https://github.com/peasannaakkalareddy/tf-module-app.git"
+
+  for_each          = var.app
+  instance_type     = each.value["instance_type"]
+  name              = each.value["name"]
+  desired_capacity  = each.value["desired_capacity"]
+  max_size          = each.value["max_size"]
+  min_size          = each.value["min_size"]
+  app_port          = each.value["app_port"]
+  listener_priority = each.value["listener_priority"]
+  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+  lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
+  parameters        = each.value["parameters"]
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+  listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
+
+
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+  #tags         = merge(local.tags, { Monitor = "true" })
+  tags         = local.tags
+  domain_name  = var.domain_name
+  domain_id    = var.domain_id
+  kms_arn      = var.kms_arn
+  monitor_cidr = var.monitor_cidr
+
+}
 module "docdb" {
   source = "git::https://github.com/peasannaakkalareddy/tf-module-docdb.git"
   for_each       = var.docdb
