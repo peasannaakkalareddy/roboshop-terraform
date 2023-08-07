c59eaf72f880b06f685da564a57b5c9459a95821
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-08-07 13:25:34 +0530

    roboshop - ansible

--- main.tf
+++ main.tf
@@ -128,40 +128,40 @@ module "rabbitmq" {
 #  vpc_id = local.vpc_id
 #}
 #
-##Load Runner
-#data "aws_ami" "ami" {
-#  most_recent      = true
-#  name_regex       = "Centos-8-DevOps-Practice"
-#  owners           = ["973714476881"]
-#}
-#
-#resource "aws_instance" "load" {
-#  ami = data.aws_ami.ami.id
-#  instance_type = "t3.small"
-#  vpc_security_group_ids = ["sg-0266fc3fc1e7aea54"]
-#  tags = {
-#    Name = "load-runner"
-#  }
-#}
-#
-#resource "null_resource" "load" {
-#  provisioner "remote-exec" {
-#
-#    connection {
-#      host     = aws_instance.load.private_ip
-#      user     = "root"
-#      password = "DevOps321"
-#    }
-#
-#    inline = [
-#      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | bash",
-#      "docker pull robotshop/rs-load"
-#    ]
-#
-#
-#  }
-#}
-#
+#Load Runner
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "load" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0266fc3fc1e7aea54"]
+  tags = {
+    Name = "load-runner"
+  }
+}
+
+resource "null_resource" "load" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.load.private_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | bash",
+      "docker pull robotshop/rs-load"
+    ]
+
+
+  }
+}
+
 module "eks" {
   source             = "git::https://github.com/peasannaakkalareddy/tf-module-eks.git"
   ENV                = var.env
